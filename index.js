f421c7e29da27af2a46b8dc2acb812d1fb3e8cb5
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-08-12 00:03:15 +0600

    updated vercel.json

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./db/bloodDB.js";
+import { corsOptions } from "./configs/corsConfig.js";
 import authRoutes from "./routes/authentication.js";
 
 dotenv.config();
